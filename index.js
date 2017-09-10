4b53210ca2ea8aa80f20afa9ebbe6363a714ff3a
Don Justine Fontanilla <djjustinekier@gmail.com>
2017-09-10 04:36:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ function deepestChild() {
 
 function increaseRankBy(n) {
   var rank = document.getElementById('app').querySelectorAll('ul.ranked-list li');
-  var addN = parseInt(rank) + n;
+  var addN = parseInt(rank.innerHTML) + n;
   return addN;
 }
